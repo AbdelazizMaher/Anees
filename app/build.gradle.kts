c7c2925c62406bcd917f6e1517d74598da7f8ecc
Ali Kotb <AliKotb38@gmail.com>
2025-10-26 20:30:28 +0300

    Change app `applicationId`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.composeVersionanees"
+        applicationId = "com.muslim.anees"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
