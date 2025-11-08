2c6c8114afd4999d77313cd3024dbbc44e83e070
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-11-08 21:56:17 +0200

    feat: Introduce DataStore and add audio-related modules
    - Integrate DataStore by binding `DataStoreImpl` to the `DataStore` interface.
    - Add and bind new audio-related modules:
      - `AudioLocalDataSource`
      - `AudioRepository`
    - Update package paths for `LocalDataSource` and `LocalDataSourceImpl`.

--- app/src/main/java/com/muslim/anees/hilt/AppBindingModule.kt
+++ app/src/main/java/com/muslim/anees/hilt/AppBindingModule.kt
@@ -1,13 +1,19 @@
 package com.muslim.anees.hilt
 
 import com.abdok.atmosphere.data.local.sharedPreference.ISharedPreferences
-import com.muslim.anees.data.local.LocalDataSource
-import com.muslim.anees.data.local.LocalDataSourceImpl
+import com.muslim.anees.data.local.datastore.DataStore
+import com.muslim.anees.data.local.datastore.DataStoreImpl
+import com.muslim.anees.data.local.sources.LocalDataSource
+import com.muslim.anees.data.local.sources.LocalDataSourceImpl
 import com.muslim.anees.data.local.sharedpreference.SharedPreferencesImpl
+import com.muslim.anees.data.local.sources.audio.AudioLocalDataSource
+import com.muslim.anees.data.local.sources.audio.AudioLocalDataSourceImpl
 import com.muslim.anees.data.remote.RemoteDataSource
 import com.muslim.anees.data.remote.RemoteDataSourceImpl
 import com.muslim.anees.data.repository.Repository
 import com.muslim.anees.data.repository.RepositoryImpl
+import com.muslim.anees.data.repository.audio.AudioRepository
+import com.muslim.anees.data.repository.audio.AudioRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -27,9 +33,21 @@ abstract class AppBindingModule {
 
     @Binds
     @Singleton
+    abstract fun bindDataStore(
+        dataStoreImpl: DataStoreImpl
+    ): DataStore
+
+    @Binds
+    @Singleton
     abstract fun bindLocalDataSource(
         localDataSourceImpl: LocalDataSourceImpl
     ): LocalDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindAudioLocalDataSource(
+        audioLocalDataSource: AudioLocalDataSourceImpl
+    ): AudioLocalDataSource
 
     @Binds
     @Singleton
@@ -43,6 +61,9 @@ abstract class AppBindingModule {
         repositoryImpl: RepositoryImpl
     ): Repository
 
-
-
+    @Binds
+    @Singleton
+    abstract fun bindAudioRepository(
+        audioRepository: AudioRepositoryImpl
+    ): AudioRepository
 }
